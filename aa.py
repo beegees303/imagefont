508495770622575daab4f1a219cddb3f45a01e04
beegees303 <87312690+beegees303@users.noreply.github.com>
2021-12-26 18:58:01 +0900

    완성

--- aa.py
+++ aa.py
@@ -1,20 +1,33 @@
 import random
 from PIL import Image, ImageDraw, ImageFont
+import os
 
-img = Image.open('james.jpg').convert("RGBA")
-txt = Image.new('RGBA', img.size, (255,255,255,0))
+path = "./"
+file_list = os.listdir(path)
+file_list = [file for file in file_list if file.endswith(".jpg")]
 
-text = "noexpect.tistory.com"
-font = ImageFont.truetype("arial.ttf", 82)
 
-d = ImageDraw.Draw(txt)
 
-width, height = img.size 
-textwidth, textheight = d.textsize(text, font)
-x=width/2-textwidth/2
-y=height/2-textheight/2
+for i in file_list:
 
-d.text((x,y), text, fill=(255,255,255,230), font=font)
 
-watermarked = Image.alpha_composite(img, txt)
-watermarked.save(r'cake_watermarked50.png')
+    img = Image.open(i).convert("RGBA")
+    file_name=os.path.basename(i)
+    
+    txt = Image.new('RGBA', img.size, (255,255,255,0))
+ 
+    text = "noexpect.tistory.com"
+    font = ImageFont.truetype("arial.ttf",int(min(img.size)/10))
+
+    d = ImageDraw.Draw(txt)
+
+    width, height = img.size 
+    textwidth, textheight = d.textsize(text, font)
+    x=width/2-textwidth/2
+    y=height/2-textheight/2
+
+    d.text((x,y), text, fill=(255,255,255,150), font=font)
+
+    watermarked = Image.alpha_composite(img, txt)
+    
+    watermarked.save(file_name+'.png')
